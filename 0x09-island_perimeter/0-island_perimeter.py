d85f6cb171dd34324ffe29839f0d0a7e64df882b
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-12-06 00:21:49 +0100

    updated code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,11 +6,11 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j] == 1:
+            if grid[i][j] == 2:
                 perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 1
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 1
+                    perimeter -= 2
 
     return perimeter
